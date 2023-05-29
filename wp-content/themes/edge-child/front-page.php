9f0230502801a49dce5e8e6e000e0ddc2b9cdd99
techyoshino <83795891+techyoshino@users.noreply.github.com>
2023-05-29 14:27:30 +0900

    2023/05/29

--- wp-content/themes/edge-child/front-page.php
+++ wp-content/themes/edge-child/front-page.php
@@ -9,6 +9,20 @@ $edge_settings = edge_get_theme_options(); ?>
 
 <?php wp_head(); ?>
 
+<!-- favicon -->
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=3">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=3">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=3">
+<link rel="manifest" href="/site.webmanifest?v=3">
+<link rel="mask-icon" href="/safari-pinned-tab.svg?v=3" color="#5bbad5">
+<link rel="shortcut icon" href="/favicon.ico?v=3">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+
+<!-- End favicon -->
+
+
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
